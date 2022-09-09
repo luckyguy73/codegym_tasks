83d7a767f139b2a222325644d95eeb3de3913a78
ashton_morgan <luckyguy73@yahoo.com>
2022-09-08 21:36:49 -0500

    task2503

---
+++ 3.JavaMultithreading/src/com/codegym/task/task25/task2503/Column.java
@@ -0,0 +1,75 @@
+package com.codegym.task.task25.task2503;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public enum Column implements Columnable {
+    Customer("Customer"),
+    BankName("Bank Name"),
+    AccountNumber("Account Number"),
+    Amount("Available Amount");
+
+    private String columnName;
+
+    private static int[] realOrder;
+
+    Column(String columnName) {
+        this.columnName = columnName;
+    }
+
+    /**
+     * Specifies a new column display order, which is saved in the realOrder array.
+     * realOrder[enum index] = display order; -1, if the column is not displayed.
+     *
+     * @param newOrder new order for displaying the columns in the table
+     * @throws IllegalArgumentException if a column is repeated
+     */
+    public static void configureColumns(Column... newOrder) {
+        realOrder = new int[values().length];
+        for (Column column : values()) {
+            realOrder[column.ordinal()] = -1;
+            boolean isFound = false;
+
+            for (int i = 0; i < newOrder.length; i++) {
+                if (column == newOrder[i]) {
+                    if (isFound) {
+                        throw new IllegalArgumentException("Column '" + column.columnName + "' is already configured.");
+                    }
+                    realOrder[column.ordinal()] = i;
+                    isFound = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * Calculates and returns the list of columns to display in a configurable order (see the configureColumns method)
+     * Uses the realOrder.
+     *
+     * @return list of columns
+     */
+    public static List<Column> getVisibleColumns() {
+        List<Column> result = new LinkedList<>();
+        for (int i = 0; i < realOrder.length; ++i)
+            for (int j = 0; j < realOrder.length; ++j)
+                if (realOrder[j] == i) result.add(Column.values()[j]);
+
+        return result;
+    }
+
+    @Override
+    public String getColumnName() {
+        return this.columnName;
+    }
+
+    @Override
+    public boolean isShown() {
+        return !(Column.realOrder[ordinal()] == -1);
+    }
+
+    @Override
+    public void hide() {
+        Column.realOrder[ordinal()] = -1;
+    }
+
+}
